abc09a6cfcf20d39a4dbfdbe2ffe82bcb6f99a07
sabakhader <149940627+sabakhader@users.noreply.github.com>
2023-12-29 18:29:37 +0300

    SecondUpdate
    su

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,50 +1,80 @@
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState("");
+  const apiKey = "1b56f07543mshcaecd24d22689cdp1345aajsnace1b40043f1";
 
-  const apiKey = "37ab3fa5b1b8ab2a3bcb8aa5d9c2bb95";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`;
+  const searchloc = async (event) => {
+    if (event.key === "Enter") {
+      const options = {
+        method: "GET",
+        url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
+        params: {
+          q: location,
+          days: "3",
+        },
+        headers: {
+          "X-RapidAPI-Key": apiKey,
+          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+        },
+      };
 
-  const searchloc = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
+      try {
+        const response = await axios.request(options);
         setData(response.data);
         console.log(response.data);
-      });
-      setLocation('');
-    }
-  }
-  
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
 
+      setLocation("");
+    }
+  };
+
+  const renderForecast = () => {
+    if (!data || !data.forecast) {
+      return null;
+    }
+
+    return data.forecast.forecastday.map((day) => (
+      <div key={day.date_epoch} className="forecast-day">
+        <p>{day.date}</p>
+        <img src={day.day.condition.icon} alt={day.day.condition.text} />
+        <p className="bold">{day.day.avgtemp_c} °C</p>
+        <p>{day.day.condition.text}</p>
+      </div>
+    ));
+  };
 
 
   return (
     <div className="app">
       <div className="search">
-      <input type="text" placeholder="Enter location" value={location}
-      onChange={event => setLocation(event.target.value)} onKeyUp={searchloc}></input>
-     
-
+        <input
+          type="text"
+          placeholder="Enter location"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyUp={searchloc}
+        ></input>
       </div>
-      
+
       <div className="container">
         <div className="top">
-          <div className="location"><p>{data.name}</p></div>
+          <div className="location">
+            <p>{data.location ? data.location.name : ""}</p>
+          </div>
           <div className="temp">
-            {data.main?<h1>{data.main.temp.toFixed()}  ْF </h1>: null}
-            </div>
-          <div className="description">{data.weather?<p>{data.weather[0].main}</p>:null}</div>
+            {data.current ? <h1>{data.current.temp_c} °C </h1> : null}
+          </div>
+          <div className="description">
+            {data.current ? <p>{data.current.condition.text}</p> : null}
+          </div>
         </div>
         <div className="bottom">
-          <div className="feels"> { data.main ?<p className="bold">{data.main.feels_like.toFixed()} ْF</p> :null}
-          <p>Feels Like</p></div>
-          <div className="humidity"> { data.main?<p className="bold">{data.main.humidity}%</p>:null}
-          <p>Humidity</p></div>
-          <div className="winds"> {data.wind?<p className="bold">{data.wind.speed.toFixed()} MPH</p>:null}
-          <p>Winds Speed</p></div>
+        {renderForecast()}
         </div>
       </div>
     </div>
@@ -52,4 +82,3 @@ function App() {
 }
 
 export default App;
-
